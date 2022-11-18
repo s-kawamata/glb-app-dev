e77cf6f69fff67488226557bb3b7b7566b311d60
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2022-11-18 18:14:32 +0900

    Update user_info.py

--- docker-apache-python/scripts/user_info.py
+++ docker-apache-python/scripts/user_info.py
@@ -1,2 +1,10 @@
 salesforce_id = "s_kawamata@ap-com.co.jp"
 salesforce_passwd = "19921107Wanko"
+
+#ログイン情報
+#salesforce_id = "k_fujihira@ap-com.co.jp"
+#salesforce_passwd = "613457Frontier"
+
+#勤怠情報
+#departure_station = "都賀" #「~"駅"」は含めなくても大丈夫です
+#destination_station = "田町" #「~"駅"」は含めなくても大丈夫です

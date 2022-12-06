105a1f9c6ce61ca997fca0ad543a9ae9e25d75b4
川俣 州生 <s_kawamata@IT-PC-2001-0418.local>
2022-12-06 10:11:29 +0900

    modify user_info.py

--- docker-apache-python/scripts/user_info.py
+++ docker-apache-python/scripts/user_info.py
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
-#salesforce_id = "s_kawamata@ap-com.co.jp"
-#salesforce_passwd = "19921107Wanko"
-
 #slack token
 slack_token = 'xoxp-296963997159-1226278144609-4430186888416-18b8d5389cffc2c3536537ab1e02abb4'#ここにslack tokenを張り付けてください
 
 #ログイン情報
-salesforce_id = "k_fujihira@ap-com.co.jp"
-salesforce_passwd = "613457Frontier"
+salesforce_id = "XXXX@ap-com.co.jp" #salseforceのユーザID(メールアドレス)
+salesforce_passwd = "XXXX" #salseforceのパスワード
 
 #勤怠情報
-departure_station = "都賀" #「~"駅"」は含めなくても大丈夫です
-destination_station = "田町" #「~"駅"」は含めなくても大丈夫です
+departure_station = "XX" #「~"駅"」は含めなくても大丈夫です
+destination_station = "XX" #「~"駅"」は含めなくても大丈夫です
 syusya_time = "x時" #出社時刻を入力してください
 
 

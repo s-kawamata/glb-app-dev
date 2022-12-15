85a30e3c69e25a68637d71ed2362911f3fc57918
川俣 州生 <s_kawamata@IT-PC-2001-0418.local>
2022-12-15 17:41:13 +0900

    add  glb-auto-app-mgr

--- glb-auto-app-mgr/scripts/user_info.py
+++ glb-auto-app-mgr/scripts/user_info.py
@@ -1,11 +1,5 @@
-#slack token
-slack_token = 'xoxp-296963997159-1226278144609-4430186888416-18b8d5389cffc2c3536537ab1e02abb4'#ここにslack tokenを張り付けてください
 
-salesforce_id = "s_kawamata@ap-com.co.jp"
-salesforce_passwd = "19921107Wanko"
-
-#勤怠情報
-departure_station = "逗子" #「~"駅"」は含めなくても大丈夫です
-destination_station = "神田" #「~"駅"」は含めなくても大丈夫です
-syusya_time = "9:30" #出社時刻を入力してください
+#salseforceログインID&PW 
+salesforce_id = "XXXX@ap-com.co.jp" #salseforceのログインIDを記述して下さい
+salesforce_passwd = "XXXX"          #salseforceのパスワードを記述して下さい
 

5bb76ccc6210fe00f138c0821336299b2b49a2f9
Akatsuka <akatsuka@ap-com.co.jp>
2022-12-23 18:43:57 +0900

    works on Akatsuka mac

--- glb-auto-app-mgr/scripts/user_info.py
+++ glb-auto-app-mgr/scripts/user_info.py
@@ -1,5 +1,12 @@
 
-#salseforceログインID&PW 
-salesforce_id = "XXXX@ap-com.co.jp" #salseforceのログインIDを記述して下さい
-salesforce_passwd = "XXXX"          #salseforceのパスワードを記述して下さい
+#slack token
+slack_token = 'xxxxxxxxxxxxx'#ここにslack tokenを張り付けてください
 
+#salseforceログインID&PW 
+salesforce_id = "xxxxxxxxxxxxx"
+salesforce_passwd = "xxxxxxxxxxxxx"
+
+#勤怠情報
+departure_station = "武蔵中原" #「~"駅"」は含めなくても大丈夫です
+destination_station = "神田" #「~"駅"」は含めなくても大丈夫です
+syusya_time = "9:30" #出社時刻を入力してください

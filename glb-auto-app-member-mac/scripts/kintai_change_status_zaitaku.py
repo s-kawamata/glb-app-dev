52c0f102207ae836fb22c9ae746e3cbd5ab60eb5
Takumi Akatsuka <akatsuka@ap-com.co.jp>
2023-01-20 18:42:58 +0900

    modified member-mac

--- glb-auto-app-member-mac/scripts/kintai_change_status_zaitaku.py
+++ glb-auto-app-member-mac/scripts/kintai_change_status_zaitaku.py
@@ -1,10 +1,13 @@
+import user_info
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from selenium.webdriver.support.ui import Select
-import user_info
+import datetime
+import requests
+from selenium.webdriver import DesiredCapabilities
 
 
 #CHROMEDRIVER = "C:\chromedriver.exe"

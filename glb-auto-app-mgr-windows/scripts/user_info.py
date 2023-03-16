685efd6407e276c07de9291adfa7a6d5c908852a
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-03-16 14:09:12 +0900

    Update user_info.py

--- glb-auto-app-mgr-windows/scripts/user_info.py
+++ glb-auto-app-mgr-windows/scripts/user_info.py
@@ -1,6 +1,6 @@
 
 #slack token
-slack_token = 'xoxp-296963997159-1226278144609-4760576575920-56e4aed8493536b4629c6601815b4887'
+slack_token = 'xoxp-296963997159-1226278144609-4760576575920-56e4aed8493536b4629c6601815b4887'#akatsuka_testのトークン
 
 #salseforceログインID&PW 
 salesforce_id = "akatsuka@ap-com.co.jp"
@@ -9,15 +9,3 @@ salesforce_passwd = "Atakumi5125"
 departure_station = "武蔵中原" #「~"駅"」は含めなくても大丈夫です
 destination_station = "神田" #「~"駅"」は含めなくても大丈夫です
 #syusya_time = "xx:xx" #出社時刻を入力してください
-
-
-# #slack token
-# slack_token = 'xoxp-296963997159-1226278144609-4760576575920-56e4aed8493536b4629c6601815b4887'
-
-# #salseforceログインID&PW 
-# salesforce_id = "k_fujihira@ap-com.co.jp"
-# salesforce_passwd = "613457Frontier"
-# #勤怠情報
-# departure_station = "扇大橋" #「~"駅"」は含めなくても大丈夫です
-# destination_station = "神田" #「~"駅"」は含めなくても大丈夫です
-# #syusya_time = "xx:xx" #出社時刻を入力してください

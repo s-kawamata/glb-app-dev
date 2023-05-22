83a6ea45bbf67bd3a374bb464c36b42ed897695e
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-05-22 23:20:33 +0900

    Update user_info.py

--- glb-auto-app-member-windows/scripts/user_info.py
+++ glb-auto-app-member-windows/scripts/user_info.py
@@ -1,10 +1,12 @@
 
 #slack token
 slack_token = 'ここにslack tokenを張り付けてください'
+
 #salseforceログインID&PW 
 salesforce_id = "ここにユーザIDを入力してください"
 salesforce_passwd = "ここにパスワードを入力してください"
+
 #勤怠情報
 departure_station = "ここに出発駅を入力してください" #「~"駅"」は含めなくても大丈夫です
 destination_station = "ここに到着駅を入力してください" #「~"駅"」は含めなくても大丈夫です
-#syusya_time = "xx:xx" #出社時刻を入力してください
+syusya_time = "xx:xx" #出社時刻を入力してください

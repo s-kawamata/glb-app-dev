5d6db12c759be6888cbdcec9858180e73a9dd394
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-05-22 23:30:03 +0900

    Update user_info.py

--- glb-auto-app-member-windows/scripts/user_info.py
+++ glb-auto-app-member-windows/scripts/user_info.py
@@ -9,4 +9,3 @@ salesforce_passwd = "ここにパスワードを入力してください"
 #勤怠情報
 departure_station = "ここに出発駅を入力してください" #「~"駅"」は含めなくても大丈夫です
 destination_station = "ここに到着駅を入力してください" #「~"駅"」は含めなくても大丈夫です
-syusya_time = "xx:xx" #出社時刻を入力してください

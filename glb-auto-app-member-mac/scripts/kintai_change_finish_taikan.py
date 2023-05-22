2328d833100f89c1bd193e714d117569887d35a0
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-05-22 23:22:17 +0900

    Update kintai_change_finish_taikan.py

--- glb-auto-app-member-mac/scripts/kintai_change_finish_taikan.py
+++ glb-auto-app-member-mac/scripts/kintai_change_finish_taikan.py
@@ -14,7 +14,7 @@ from selenium.webdriver.support import expected_conditions
 
 
 TOKEN = user_info.slack_token
-CHANNEL = 'akatsuka_test'
+CHANNEL = 'glb_kintai'
 
 url = "https://slack.com/api/chat.postMessage"
 headers = {"Authorization": "Bearer "+TOKEN}

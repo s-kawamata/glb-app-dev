97535a4e73407c5e2d6c1d73fed05c1b5448bb6f
FujihiraKazuaki <117697346+FujihiraKazuaki@users.noreply.github.com>
2023-05-23 19:48:21 +0900

    Update user_info.py

--- glb-auto-app-member-windows/scripts/user_info.py
+++ glb-auto-app-member-windows/scripts/user_info.py
@@ -1,5 +1,5 @@
 #slack token
-slack_token = 'xoxp-296963997159-1226278144609-5324206951584-29777481fb27ef27d2381743b3392465'
+slack_token = 'ここにtokenを記入してください'
 
 #salseforceログインID&PW 
 salesforce_id = "ここにユーザIDを入力してください"
